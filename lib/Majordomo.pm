241bf2194781fc1ffdb0538ad9fee7c7224c367d
tibbs <>
1998-04-03 05:38:08 +0000

    Bump version number.

--- lib/Majordomo.pm
+++ lib/Majordomo.pm
@@ -77,7 +77,7 @@ simply not exist.
 package Majordomo;
 
 @ISA = qw(Mj::Access Mj::Token Mj::MailOut Mj::Resend Mj::Inform);
-$VERSION = "0.0199804020";
+$VERSION = "0.0199804021";
 
 use strict;
 no strict 'refs';
