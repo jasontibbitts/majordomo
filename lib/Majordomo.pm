2960cd413c6106f25a6198369c3a3677e57855d2
tibbs <>
1999-02-21 07:27:23 +0000

    Fix version number.

--- lib/Majordomo.pm
+++ lib/Majordomo.pm
@@ -77,7 +77,7 @@ simply not exist.
 package Majordomo;
 
 @ISA = qw(Mj::Access Mj::Token Mj::MailOut Mj::Resend Mj::Inform);
-$VERSION = "0.119902200";
+$VERSION = "0.1199902200";
 $unique = 'AAA';
 
 use strict;
