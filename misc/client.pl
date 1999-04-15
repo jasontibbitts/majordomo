895f94cea879cf1a3b0d4f334bfb90d12d4ed3ec
tibbs <>
1999-04-15 06:54:15 +0000

    Queueing fixes for Solaris. Early support for domain and list priorities in the queueing system.

--- misc/client.pl
+++ misc/client.pl
@@ -1,31 +1,5 @@
 #!/usr/bin/perl -w
 
-# Client test program
-
-# Figure out queue directory based on args (domain/list/function)
-
-# Generate a unique temporary filename.
-
-# Copy message into file.
-
-# Now we're safe; even if something bombs the message has been saved and
-# will be picked up.
-
-# Make sure server is active.
-
-# Send message to server somehow.
-
-
-
-# Test version:
-
-# Start server if necessary.
-
-# Say hello.
-
-# Get result, exit.
-
-
 sub logmsg { print STDERR "C $$: @_\n" }
 
 use Socket;
@@ -47,6 +21,8 @@ if (!($ok = connect(SOCK, sockaddr_un($rendezvous)))) {
 
 # Poll with linear backoff until server starts up
 for ($i = 0; $i < 10 || die "connect: $!"; $i++) {
+  close SOCK;
+  socket(SOCK, PF_UNIX, SOCK_STREAM, 0)       || die "socket: $!";
   $ok ||= connect(SOCK, sockaddr_un($rendezvous));
   last if $ok;
   sleep $i;
