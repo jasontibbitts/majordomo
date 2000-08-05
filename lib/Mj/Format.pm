bef1ba0ab73919ffc2a66425945488e42776b0a1
csf <>
2000-08-05 15:55:51 +0000

    (report) Reverse user and victim.

--- lib/Mj/Format.pm
+++ lib/Mj/Format.pm
@@ -675,7 +675,7 @@ sub report {
     last unless scalar @$chunk;
     for $data (@$chunk) {
       # Remove the comment from the victim's address.
-      $victim = $data->[1] eq 'post'? $data->[3] : $data->[2];
+      $victim = $data->[1] eq 'post'? $data->[2] : $data->[3];
       $victim =~ s/.*<([^>]+)>.*/$1/;
       @tmp = localtime($data->[9]);
       $end = strftime("%d %b %H:%M", @tmp);
