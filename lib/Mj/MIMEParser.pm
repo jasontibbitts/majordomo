9b19b4ab9975f8d644e888401d27cf6ef607ae1c
csf <>
2000-09-11 21:53:58 +0000

    (collect_data) Return header and body information about a MIME entity. (_r_ct_lines) Count the lines in the body parts of a MIME entity.

--- lib/Mj/MIMEParser.pm
+++ lib/Mj/MIMEParser.pm
@@ -31,6 +31,93 @@ sub output_path {
     "$dir/$name";  
 }
 
+use AutoLoader 'AUTOLOAD';
+1;
+__END__
+
+=head2 collect_data(entity)
+
+Return information about a MIME entity:
+
+body_lines quoted date from subject refs;
+
+=cut
+use Date::Parse;
+sub collect_data {
+  my ($entity, $qp) = @_;
+  my (@rcv, @refs, $tmp);
+  return unless $entity;
+  my ($data) = {
+                 'body_lines'  => 0,
+                 'date'        => time,
+                 'from'        => '',
+                 'quoted'      => 0,
+                 'refs'        => '',
+                 'subject'     => '',
+               };
+  my ($head) = $entity->head;
+  return unless $head;
+
+  # Obtain references
+  @refs = ();
+  $tmp = $head->get('references') || '';
+  while ($tmp =~ s/<([^>]*)>//) {
+    push @refs, $1;
+  }
+  $tmp = $head->get('in-reply-to') || '';
+  while ($tmp =~ s/<([^>]*)>//) {
+    push @refs, $1;
+  }
+  $data->{'refs'} = join "\002", @refs;
+ 
+  $data->{'subject'} = $head->get('subject') || ''; 
+  chomp $data->{'subject'};
+
+  # Convert the message date into a time value.
+  # Use the earliest Received header, or the Date header.
+  $tmp = '';
+  @rcv = $head->get('received');
+  if (@rcv) {
+    @rcv = split /\s*;\s*/, $rcv[-1];
+    $tmp = $rcv[-1];
+  }
+  $tmp ||= $head->get('date');
+  chomp $tmp;
+  $tmp = &str2time($tmp);
+  $tmp = time unless ($tmp > 0 and $tmp < time);
+  $data->{'date'} = $tmp;
+  
+  chomp($data->{'from'} = $head->get('from') ||
+        $head->get('apparently-from'));
+
+  _r_ct_lines($entity, $data, $qp);
+  # Account for separator
+  $data->{'body_lines'}--;
+
+  $data;
+} 
+   
+
+sub _r_ct_lines {
+  my ($entity, $data, $qp) = @_;
+  my (@parts) = $entity->parts;
+  my ($body, $line);
+  if (@parts) {
+    for ($i=0; $i<@parts; $i++) {
+      _r_ct_lines($parts[$i], $data, $qp);
+    }
+    return;
+  }
+  $body = $entity->bodyhandle->open('r');
+  return unless $body;
+
+  # Iterate over the lines
+  while ($line = $body->getline) {
+    $data->{body_lines}++;
+    $data->{quoted_lines}++ if Majordomo::_re_match($qp, $line);
+  }
+}
+
 =head1 COPYRIGHT
 
 Copyright (c) 1997, 1998 Jason Tibbitts for The Majordomo Development
