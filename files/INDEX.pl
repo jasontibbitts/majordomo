2e093993cc6ac7300603c9ec0f6823d8c3e8ed61
csf <>
2002-05-30 00:28:11 +0000

    Add "noweb" versions of the stock welcome messages.

--- files/INDEX.pl
+++ files/INDEX.pl
@@ -2,12 +2,14 @@
 # web interfaces are being used.
 
 $noweb_files = {
-  'en/confirm' => 1,
-  'en/consult' => 1,
-  'en/delay' => 1,
-  'en/token_remind' => 1,
-  'de/confirm' => 1,
-  'de/consult' => 1,
+  'en/confirm'          => 1,
+  'en/consult'          => 1,
+  'en/delay'            => 1,
+  'en/registered'       => 1,
+  'en/token_remind'     => 1,
+  'en/welcome'          => 1,
+  'de/confirm'          => 1,
+  'de/consult'          => 1,
   'de/informal/confirm' => 1,
   'de/informal/consult' => 1,
 };
