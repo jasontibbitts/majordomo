4697c7eae4344b5ec5bfa1af0e069780b19754c6
csf <>
2003-11-05 02:29:02 +0000

    Automatically create the scripts directory.

--- setup/setup_func.pl
+++ setup/setup_func.pl
@@ -40,6 +40,7 @@ sub create_dirs {
   safe_mkdir($tmp,         0777 & ~oct($um), $uid, $gid);dot();
   safe_mkdir("$tmp/locks", 0777 & ~oct($um), $uid, $gid);dot();
 
+  safe_mkdir("$l/scripts",    0777 & ~oct($um), $uid, $gid);dot();
   safe_mkdir("$l/LIB",        0777 & ~oct($um), $uid, $gid);dot();
   safe_mkdir("$l/SITE",       0777 & ~oct($um), $uid, $gid);dot();
   safe_mkdir("$l/SITE/files", 0777 & ~oct($um), $uid, $gid);dot();
